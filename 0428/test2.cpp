98c9369a6ede20d7e9740daaddade541cf215687
w.y.ff <walker.y.f@outlook.com>
2017-04-29 10:45:05 +0800

    0429

--- 0428/test2.cpp
+++ 0428/test2.cpp
@@ -1,44 +1,50 @@
  class base
 {
-	char *name;
-	int age;
+// protected:
+	char * _name;
+	int _age;
 public:
 	base(){}
-	void setname(char arr[]);
-	void setage(int age);
-	char* getname();
-	int getage();
+	void setname(char arr[]) { _name = arr;} 
+	void setage(int age) { _age = age;} 
+	char* getname() {return _name;} 
+	int getage() {return _age;};
 };
 
 
 
 class leader:virtual public base{
+// protected:
 	char * _job;
 	char * _dep;
 public:
-	setjob(char job[]);
-	setdep(char dep[]);
+	void setjob(char job[]){_job = job;} ;
+	void setdep(char dep[]) {_dep = dep;} ;
+	char * getjob(){return _job;} ;
+	char * getdep(){return _dep;} ;
 };
 
 class engineer:virtual public base{
+// protected:
 	char * _major;
 	char * _prof;
 public:
-	setjob(char major[]);
-	setdep(char prof[]);
+	void setmajor(char major[]){_major = major;} 
+	void setprof(char prof[]){_prof = prof;} 
+	char * getmajor(){return _major;} 
+	char * getprof(){return _prof;} 
 };
 
-class leader:public leader, public engineer{
+class chairman: public leader, public engineer{
 
 };
-
-
+ 
 #include<iostream>
-#include<string.h>
+#include<string.h>  
 #include<cstring>
-#include <fstream>
+#include <fstream> 
 using namespace std;
-#include "source.h"
+// #include "source.h"
 int main()
 {
 	//freopen("in.txt", "r", stdin);
@@ -57,7 +63,6 @@ int main()
 		man.setdep(dep);
 		man.setmajor(major);
 		man.setprof(prof);
-		
 		cout<<"name: "<<man.getname()<<" age: "<<man.getage()<<" dep: "<<man.getdep()
 			<<" job: "<<man.getjob()<<endl;
 		cout<<"prof: "<<man.getprof()<<" major: "<<man.getmajor()<<endl;
